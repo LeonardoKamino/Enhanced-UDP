12392be25862a229ecb3d1a493b9e2adf8fbd31f
Maddy Paulson <madeline1paulson@gmail.com>
2024-02-21 19:19:04 -0800

    Created initial UDP based protocol for the sender and receiver.
    Co-authored-by: Leonardo Kamino Barros <LeonardoKamino@users.noreply.github.com>

--- src/receiver.c
+++ src/receiver.c
@@ -11,10 +11,86 @@
 #include <pthread.h>
 #include <errno.h>
 
-void rrecv(unsigned short int myUDPport, 
-            char* destinationFile, 
-            unsigned long long int writeRate) {
+#define BUFFER_SIZE 1024
 
+void printBufferContents(const char *buffer, ssize_t length) {
+    printf("Buffer contents:\n");
+    printf("ASCII: ");
+    for (ssize_t i = 0; i < length; i++) {
+        // Print as a character if it's printable, otherwise print '.'
+        printf("%c", (buffer[i] >= 32 && buffer[i] <= 126) ? buffer[i] : '.');
+    }
+    printf("\nHEX: ");
+    for (ssize_t i = 0; i < length; i++) {
+        // Print each byte in hexadecimal format
+        printf("%02X ", (unsigned char)buffer[i]);
+    }
+    printf("\n\n");
+}
+
+void rrecv(unsigned short int myUDPport, char* destinationFile, unsigned long long int writeRate) {
+    
+    // initial parameters
+    int sockDescriptor;
+    struct sockaddr_in myAddr;
+    char buffer[BUFFER_SIZE];
+    ssize_t receivedBytes;
+    FILE *file;
+    unsigned long long int bytesWritten = 0;
+
+    // create UDP socket
+    sockDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockDescriptor < 0){
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // setup
+    memset(&myAddr, 0, sizeof(myAddr));
+    myAddr.sin_family = AF_INET;
+    myAddr.sin_port = htons(myUDPport);
+    myAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    fprintf(stdout, "hello 1\n");
+
+    // bind socket
+    if(bind(sockDescriptor, (struct sockaddr *)&myAddr, sizeof(myAddr)) < 0) {
+        perror("Bind failed");
+        close(sockDescriptor);
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stdout, "hello 2\n");
+
+    // open file
+    file = fopen(destinationFile, "wb");
+    if(file == NULL){
+        perror("Failed to open destination file for writing.");
+        close(sockDescriptor);
+        exit(EXIT_FAILURE);
+    }
+
+
+    while(1){
+        receivedBytes = recvfrom(sockDescriptor, buffer, BUFFER_SIZE, 0, NULL, NULL);
+
+        if(receivedBytes < 0){
+            perror("recvfrom failed");
+            break;
+        }
+
+        fprintf(stdout, "%d\n", receivedBytes);
+        printBufferContents(buffer, receivedBytes);
+        fwrite(buffer, 1, receivedBytes, file);
+
+        bytesWritten += receivedBytes;
+        fclose(file);
+        break;
+
+
+        // implement logic for writerate > 0 here
+    }
+
+    fclose(file);
+    close(sockDescriptor);
 }
 
 int main(int argc, char** argv) {
@@ -24,6 +100,7 @@ int main(int argc, char** argv) {
     // command line.
 
     unsigned short int udpPort;
+    char* filename = NULL;
 
     if (argc != 3) {
         fprintf(stderr, "usage: %s UDP_port filename_to_write\n\n", argv[0]);
@@ -31,4 +108,7 @@ int main(int argc, char** argv) {
     }
 
     udpPort = (unsigned short int) atoi(argv[1]);
+    filename = argv[2];
+
+    rrecv(udpPort, filename, 0);
 }
--- src/sender.c
+++ src/sender.c
@@ -11,11 +11,56 @@
 #include <pthread.h>
 #include <errno.h>
 
+#define BUFFER_SIZE 1024
+
 void rsend(char* hostname, 
             unsigned short int hostUDPport, 
             char* filename, 
             unsigned long long int bytesToTransfer) 
 {
+    // initial parameters
+    int sockDescriptor;
+    int seqNum = 0;
+    struct sockaddr_in destAddr;
+    char buffer[BUFFER_SIZE];
+    ssize_t readBytes, sentBytes;
+    FILE *file;
+    unsigned long long int totalBytesSent = 0;
+
+    // create UDP socket
+    sockDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockDescriptor < 0){
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&destAddr, 0, sizeof(destAddr));
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(hostUDPport);
+    if(inet_pton(AF_INET, hostname,&destAddr.sin_addr) <= 0) {
+        perror("Invalid address.");
+        close(sockDescriptor);
+        exit(EXIT_FAILURE);
+    }
+
+    if((file = fopen(filename, "rb")) == NULL) {
+        perror("Opening file failed");
+        close(sockDescriptor);
+        exit(EXIT_FAILURE);
+    }
+
+    while(totalBytesSent < bytesToTransfer && (readBytes = fread(buffer, 1, BUFFER_SIZE, file)) > 0){
+        sentBytes = sendto(sockDescriptor, buffer, readBytes, 0, (struct sockaddr *)&destAddr, sizeof(destAddr));
+        if(sentBytes < 0) {
+            perror("Error sending file");
+            break;
+        }
+        totalBytesSent += sentBytes;
+    }
+
+    fclose(file);
+    close(sockDescriptor);
+
 
 }
 
@@ -27,6 +72,7 @@ int main(int argc, char** argv) {
     int hostUDPport;
     unsigned long long int bytesToTransfer;
     char* hostname = NULL;
+    char* filename = NULL;
 
     if (argc != 5) {
         fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n\n", argv[0]);
@@ -35,6 +81,9 @@ int main(int argc, char** argv) {
     hostUDPport = (unsigned short int) atoi(argv[2]);
     hostname = argv[1];
     bytesToTransfer = atoll(argv[4]);
+    filename = argv[3];
 
-    return (EXIT_SUCCESS);
+    rsend(hostname, hostUDPport, filename, bytesToTransfer);
+
+    return (EXIT_SUCCESS); 
 }
